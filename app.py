079dd9020b28c6d6794b0e4b1501e2a8206e5d8f
mayukhdeb <mayukhmainak2000@gmail.com>
2021-05-01 19:28:32 +0530

    app: offload stuff into twitter_stalker

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from urllib.parse import quote
 
 import dash
 import dash_core_components as dcc
-import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 from dash_table import DataTable
@@ -17,234 +16,27 @@ import advertools as adv
 from dash.exceptions import PreventUpdate
 import plotly.graph_objs as go
 
+from twitter_stalker.html_components import Layout
+
+from twitter_stalker.constants import (
+    auth_params, 
+    twitter_lang_metadata_filename,
+    exclude_columns,
+    regex_dict,
+    phrase_len_dict,
+    img_base64
+)
+
 logging.basicConfig(level=logging.INFO)
 
-auth_params = {
-    'app_key': '',
-    'app_secret': '',
-    'oauth_token': '',
-    'oauth_token_secret': '',
-}
-
 adv.twitter.set_auth_params(**auth_params)
-
-img_base64 = base64.b64encode(open('./logo.png', 'rb').read()).decode('ascii')
-
-twtr_lang_df = pd.read_csv('twitter_lang_df.csv')
-twtr_lang_df = twtr_lang_df.sort_values('name')
-lang_options = [{'label': loc_name, 'value': code}
-                for loc_name, code in
-                zip(twtr_lang_df['name'] + ' | ' + twtr_lang_df['local_name'],
-                    twtr_lang_df['code'])]
-
-exclude_columns = ['tweet_entities', 'tweet_geo', 'user_entities',
-                   'tweet_coordinates', 'tweet_metadata',
-                   'tweet_extended_entities', 'tweet_contributors',
-                   'tweet_display_text_range', 'tweet_user', 'tweet_place',
-                   'tweet_truncated']
-
-regex_dict = {'Emoji': adv.emoji.EMOJI_RAW,
-              'Mentions': adv.regex.MENTION_RAW,
-              'Hashtags': adv.regex.HASHTAG_RAW,}
-
-phrase_len_dict = {'Words': 1,
-                   '2-word Phrases': 2,
-                   '3-word Phrases': 3}
-
-def get_str_dtype(df, col):
-    """Return dtype of col in df"""
-    dtypes = ['datetime', 'bool', 'int', 'float',
-              'object', 'category']
-    for d in dtypes:
-        if d in str(df.dtypes.loc[col]).lower():
-            return d
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
 app.title = "Twitter Stalker"
 
 server = app.server
 
-app.layout = html.Div([
-    dcc.Loading(dcc.Store(id='twitter_df', storage_type='memory')),
-    html.Br(),
-    dbc.Row([
-        dbc.Col([
-            html.A([
-                html.Img(src='data:image/png;base64,' + img_base64,
-                         width=200, style={'display': 'inline-block'}),
-            ]),
-            html.Br(),
-        ], lg=2, xs=11, style={'textAlign': 'center'}),
-        dbc.Col([
-            html.Br(),
-            html.H1('Twitter Stalker: Analyze some twitter data',
-                    style={'textAlign': 'center',
-                            'color': 'white'})
-        ], lg=9, xs=11),
-    ], style={'margin-left': '1%'}),
-    html.Br(),
-    dbc.Row([
-        dbc.Col(lg=2, xs=10),
-        dbc.Col([
-           dcc.Dropdown(id='search_type',
-                        placeholder='Search Type',
-                        options=[{'label': c, 'value': c}
-                                 for c in ['Search Tweets',
-                                           'Search Users',
-                                           'Get User Timeline']])
-        ], lg=2, xs=10),
-        dbc.Col([
-            dbc.Input(id='twitter_search',
-                      placeholder='Search query'),
-        ], lg=2, xs=10),
-        dbc.Col([
-            dbc.Input(id='twitter_search_count',
-                      placeholder='Number of results', type='number'),
-
-        ], lg=2, xs=10),
-        dbc.Col([
-            dcc.Dropdown(
-                id='twitter_search_lang', placeholder='Language',
-                         options=lang_options,
-                         style={'position': 'relative', 'zIndex': 15}
-                         ),
-        ], lg=2, xs=10),
-        dbc.Col([
-            dbc.Button(id='search_button', children='Submit', outline=True, color='primary'),
-        ], lg=2, xs=10),
-    ]),
-    html.Hr(),
-    dbc.Container([
-        dbc.Col(lg=2, xs=10),
-        dbc.Tabs([
-            dbc.Tab([
-                html.Br(),
-                dbc.Row([
-                    dbc.Col([
-                        dbc.Label('Text field:',color='primary'),
-                        dcc.Dropdown(id='text_columns',
-                                     placeholder='Text Column',
-                                     value='tweet_full_text'),
-                    ], lg=3, xs=9),
-                    dbc.Col([
-                        dbc.Label('Weighted by:',color='primary'),
-                        dcc.Dropdown(id='numeric_columns',
-                                     placeholder='Numeric Column',
-                                     value='user_followers_count'),
-                    ], lg=3, xs=9),
-                    dbc.Col([
-                        dbc.Label('Elements to count:',color='primary'),
-                        dcc.Dropdown(id='regex_options',
-                                     options=[{'label': x, 'value': x}
-                                              for x in ['Words', 'Emoji',
-                                                        'Mentions', 'Hashtags',
-                                                        '2-word Phrases',
-                                                        '3-word Phrases']],
-                                     value='Words'),
-                    ], lg=3, xs=9),
-                ]),
-                html.Br(),
-                html.H2(id='wtd_freq_chart_title',
-                        style={'textAlign': 'center',
-                        'color':'white',
-                        'font':'white'}),
-                dcc.Loading([
-                    dcc.Graph(id='wtd_freq_chart',
-                              config={'displayModeBar': False},
-                              figure={'layout': {
-                                  'plot_bgcolor': '#0e0e0e',
-                                  'paper_bgcolor': '#0e0e0e',
-                                  'font': '#ffffff'
-                              }})
-                ]),
-            ], label='Text Analysis', id='text_analysis_tab'),
-            dbc.Tab([
-                html.H3(id='user_overview', style={'textAlign': 'center','color':'white'}),
-                dcc.Loading([
-                    dcc.Graph(id='user_analysis_chart',
-                              config={'displayModeBar': False},
-                              figure={'layout': {
-                                  'plot_bgcolor': '#0e0e0e',
-                                  'paper_bgcolor': '#0e0e0e',
-                                  'font': 'white',
-                                'color':'#ffffff'
-                              }})
-                ]),
-            ], tab_id='user_analysis_tab', label='User Analysis'),
-        ], id='tabs'),
-    ]),
-    html.Hr(), html.Br(),
-    dbc.Row([
-        dbc.Col(lg=3, xs=11),
-        dbc.Col(id='container_col_select',
-                children=dcc.Dropdown(id='col_select',
-                                      placeholder='Filter by:'),
-                lg=2, xs=11),
-        dbc.Col([
-            dbc.Col([
-                dbc.Container(children=dcc.RangeSlider(id='num_filter',
-                                                       updatemode='mouseup')),
-                dbc.Container(children=html.Div(id='rng_slider_vals'),
-                              style={'text-align': 'center'}),
-            ], lg=0, xs=0, id='container_num_filter',
-                style={'display': 'none'}),
-            dbc.Col(id='container_str_filter',
-                    style={'display': 'none'},
-                    children=dcc.Input(id='str_filter'), lg=0, xs=0),
-            dbc.Col(id='container_bool_filter',
-                    style={'display': 'none'},
-                    children=dcc.Dropdown(id='bool_filter',
-                                          options=[{'label': 'True',
-                                                    'value': 1},
-                                                   {'label': 'False',
-                                                    'value': 0}]),
-                    lg=0, xs=0,),
-            dbc.Col(id='container_cat_filter',
-                    style={'display': 'none'},
-                    children=dcc.Dropdown(id='cat_filter', multi=True),
-                    lg=0, xs=0),
-            dbc.Col([
-                dcc.DatePickerRange(id='date_filter'),
-            ], id='container_date_filter', style={'display': 'none','color':'white'},
-                lg=0, xs=0),
-        ], style={'width': '20%', 'display': 'inline-block'}),
-        dbc.Col(id='row_summary', lg=2, xs=11),
-        dbc.Col(html.A('Download Table', id='download_link',
-                       download="rawdata.csv", href="", target="_blank",
-                       n_clicks=0), lg=2, xs=11),
-    ], style={'position': 'relative', 'zIndex': 5}),
-    dbc.Row([
-        dbc.Col([
-            dbc.Label('Add/remove columns: ',color='primary'),
-            dcc.Dropdown(id='output_table_col_select', multi=True,
-                         value=['tweet_created_at', 'user_screen_name',
-                                'user_followers_count', 'tweet_full_text',
-                                'tweet_retweet_count']
-                         ),
-        ], lg=2, xs=11, style={'margin-left': '1%'}),
-        dbc.Col([
-            html.Br(),
-            dcc.Loading(
-                DataTable(id='table', sort_action='native',
-                        #   n_fixed_rows=1,
-                        #   filtering=False,
-                          virtualization=True,
-                          fixed_rows={'headers': True},
-                        #   style_cell_conditional=[{
-                        #       'if': {'row_index': 'odd'},
-                        #       'backgroundColor': '#eeeeee',
-                        #       'alignText':'left'}],
-                          style_header={'backgroundColor': 'rgb(30, 30, 30)'},
-                          style_cell={'width': '200px',
-                                      'font-family': 'Source Sans Pro',
-                                      'textAlign':'center',
-                                      'color':'white',
-                                      'backgroundColor':'rgb(50, 50, 50)'}),
-            ),
-        ], lg=9, xs=11, style={'position': 'relative', 'zIndex': 1,
-                               'margin-left': '1%'}),
-    ] + [html.Br() for x in range(30)]),
-], style={'backgroundColor': '#0f0f0f'})
+app.layout = Layout
 
 @app.callback([Output('text_columns', 'options'),
                Output('text_columns', 'value'),
@@ -299,6 +91,7 @@ def display_user_overview(df, search_type):
                Input('numeric_columns', 'value'),
                Input('regex_options', 'value'),
                Input('search_type', 'value')])
+
 def plot_wtd_frequency(df, text_col, num_col, regex, search_type):
     if (df is None) or (text_col is None) or (num_col is None) or \
             (search_type is None):
